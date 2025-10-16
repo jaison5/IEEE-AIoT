68c630eb9b5e6178c70886a05c3be8edf32a3a82
jaison5 <144083309+jaison5@users.noreply.github.com>
2025-10-16 18:43:42 +0800

    Update train2.py

--- code/train2.py
+++ code/train2.py
@@ -6,30 +6,30 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 assert torch.cuda.is_available(), "❌ 找不到 GPU，請確認 CUDA 與 PyTorch 安裝正確"
 torch.cuda.empty_cache()
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
-torch.backends.cudnn.benchmark = True  # ✅ 自動選擇最佳卷積演算法
+torch.backends.cudnn.benchmark = True  
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
 
-    model = YOLO('models/11n-2.pt')   # 建議使用官方 YOLO11n 初始權重
+    model = YOLO('models/11n-3.pt')   
 
     results = model.train(
-        data="yaml/20250105.yaml",  # 資料集
-        imgsz=1024,                 # ✅ 高解析度訓練，適合大顯存（1536~2048）
-        epochs=300,                 # 訓練次數
-        batch=12,                    # ✅ 建議 8~12；可依 VRAM 使用量調整
-        workers=8,                  # 多執行緒載入
+        data="yaml/20250105.yaml",  
+        imgsz=1024,                 
+        epochs=300,                 
+        batch=12,                    
+        workers=8,                  
         device=0,
-        amp=True,                   # 混合精度加速
+        amp=True,                   
         val=True,
-        pretrained=True,            # 使用預訓練權重
+        pretrained=True,           
         save_period=10,
         project='gamechess',
         name='exp',
         lr0=0.005,
         lrf=0.1,
-        patience=50,                # 容忍度高一點
-        warmup_epochs=5,            # 更穩定的初期收斂
+        patience=50,                
+        warmup_epochs=5,           
         cos_lr=True,
         augment=True,
         mosaic=0.8,
